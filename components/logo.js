fee7cb8befe6e2c949fa84f6f12a3c7a7ceeca64
Michael <michael@mclain.co.uk>
2022-07-18 19:30:29 +0100

    <edit> fixed logo to work with dark mode.

--- components/logo.js
+++ components/logo.js
@@ -17,22 +17,22 @@ const LogoBox = styled.span`
     }
 
     &:hover img {
-        transform: scale(0.2);
+        transform: scale(0.8);
     }
 `
 
 const Logo = () => {
-    const logoimg = `/images/logo${useColorModeValue('', '-dark')}.png`
-
+    const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+  
     return (
-        <Link href="/" scroll={false}>
-            <a>
-                <LogoBox>
-                    <Image src={logoimg} width={20} height={20} alt=""/>
-                </LogoBox>
-            </a>
-        </Link>
+      <Link href="/" scroll={false}>
+        <a>
+          <LogoBox>
+            <Image src={footPrintImg} width={20} height={20} alt="" />
+          </LogoBox>
+        </a>
+      </Link>
     )
-}
-
-export default Logo
+  }
+  
+  export default Logo
