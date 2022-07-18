21cab16fd1a85c250dd6dda0ad53761375836bb8
Michael <michael@mclain.co.uk>
2022-07-18 19:59:34 +0100

    <edit> added mario easter egg

--- pages/404.js
+++ pages/404.js
@@ -4,8 +4,8 @@ import { Box, Heading, Text, Container, Divider, Button } from '@chakra-ui/react
 const NotFound = () => {
     return (
         <Container>
-            <Heading as="h1">Keep Swimming...</Heading>
-            <Text>The page you&apos;re looking for was not found.</Text>
+            <Heading align="center" as="h1">Thank You Mario! But Our Princess Is In Another Castle!</Heading>
+            <Text align="center">The page you&apos;re looking for was not found.</Text>
             <Divider my={6} />
             <Box my={6} align="center">
                 <NextLink href="/" passHref>
