a67f72e84085a50010720964f71500348762c971
Michael <michael@mclain.co.uk>
2022-09-17 20:56:47 +0100

    [+] Ajusted scale of GLB model.

--- components/item.js
+++ components/item.js
@@ -50,7 +50,7 @@ const Item = () => {
             container.appendChild(renderer.domElement)
             setRenderer(renderer)
 
-            const scale = scH * 0.005 + 1
+            const scale = scH * 0.005 + 2
             const camera = new THREE.OrthographicCamera(
                 -scale,
                 scale,
