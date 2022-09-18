b46994574aeb5c6b89e4135591c5764b9e992abf
Michael <michael@mclain.co.uk>
2022-09-18 15:11:29 +0100

    [+] React-hook-Form refactor

---
+++ components/hook-form.js
@@ -0,0 +1,100 @@
+import { useForm } from 'react-hook-form'
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react'
+
+export default function HookForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
+
+  function onSubmit(values) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2))
+        resolve()
+      }, 3000)
+    })
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl my='1' isInvalid={errors.name}>
+        <FormLabel htmlFor='name'>Full Name</FormLabel>
+        <Input
+          id='name'
+          placeholder='Name'
+          {...register('name', {
+            required: 'This is required',
+            minLength: { value: 4, message: 'Minimum length should be 4' },
+          })}
+        />
+        <FormErrorMessage>
+          {errors.name && errors.name.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl my='1' isInvalid={errors.email}>
+        <FormLabel htmlFor='email'>Email Address</FormLabel>
+        <Input
+          id='email'
+          placeholder='Email Address'
+          {...register('email', {
+            required: 'Email Address is required.',
+            pattern: {
+              value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: 'Please enter a valid email',
+          },
+          })}
+          />
+          <FormErrorMessage>
+          {errors.email && errors.email.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl my='1' isInvalid={errors.tel}>
+        <FormLabel htmlFor='tel'>Phone Number</FormLabel>
+        <Input
+          id='tel'
+          placeholder='Phone Number'
+          {...register('tel', {
+            required: 'Please enter a valid phone number.',
+            minLength: 1, message: 'Minumum Length is 6 Digits.',
+            maxLength: 99, message: 'Maximum Length is 12 Digits.',
+            pattern: {
+              value: /[0-9]{4}/,
+              message: 'Please enter a valid phone number',
+          },
+          })}
+          />
+          <FormErrorMessage>
+          {errors.tel && errors.tel.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl my='1' isInvalid={errors.text}>
+        <FormLabel htmlFor='text'>Notes</FormLabel>
+        <Input
+          id='text'
+          placeholder='Enter Text Here...'
+          {...register('text', {
+            maxLength: { value: 140, message: 'Maximum length should be 140' },
+          })}
+        />
+        <FormErrorMessage>
+          {errors.text && errors.text.message}
+        </FormErrorMessage>
+      </FormControl>
+      
+      <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+        Submit
+      </Button>
+    </form>
+  )
+}
--- pages/contact.js
+++ pages/contact.js
@@ -6,13 +6,12 @@ import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { IoLogoTwitter, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5'
 import { Stack } from '@chakra-ui/react'
-//import thumbYouTube from '../public/images/links/youtube.png'
-//import thumbInkdrop from '../public/images/links/inkdrop_eyecatching.png'
 import Image from 'next/image'
 import { InputGroup, Input } from '@chakra-ui/react'
 import { InputLeftElement, InputRightElement } from '@chakra-ui/react'
-import { PhoneIcon, CheckIcon } from '@chakra-ui/icons'
+import { CheckIcon } from '@chakra-ui/icons'
 import { Textarea } from '@chakra-ui/react'
+import HookForm from '../components/hook-form'
 
 const Home = () => (
     <Layout>
@@ -21,53 +20,14 @@ const Home = () => (
                 <Heading as="h3" variant="section-title">
                     Contact Me
                 </Heading>
-
-                <Stack spacing={4}>
-                <InputGroup>
-                    <InputLeftElement
-                        pointerEvents='none'
-                        color='gray.400'
-                        fontSize='1.2em'
-                        children={<ChatIcon color='gray.400'/>}
-                    />
-                    <Input variant='outline' focusBorderColor='teal.400' placeholder='Enter Name' type='name'/>
-                    <InputRightElement children={<CheckIcon color='green.500' />} />
-                  </InputGroup>
-
-                  <InputGroup>
-                    <InputLeftElement
-                        pointerEvents='none'
-                        children='+44' color='gray.400'
-                    />
-                    <Input variant='outline' focusBorderColor='teal.400' type='tel' placeholder='Phone number' />
-                    <InputRightElement children={<CheckIcon color='green.500' />} />
-                  </InputGroup>
-
-                  <InputGroup>
-                    <InputLeftElement
-                        pointerEvents='none'
-                        color='gray.400'
-                        fontSize='1.2em'
-                        children={<EmailIcon color='gray.400' />}
-                    />
-                    <Input variant='outline' focusBorderColor='teal.400' type='email' placeholder='Enter Email' />
-                    <InputRightElement children={<CheckIcon color='green.500' />} />
-                  </InputGroup>
-
-                  <InputGroup>
-                    <Textarea variant='outline' focusBorderColor='teal.400' color='gray.400' size='md' placeholder='Extended Message' />
-                    <InputRightElement children={<CheckIcon color='green.500' />} />
-                  </InputGroup>
-                </Stack>
-                
-                <Box align="center" my={4}>
-                    <Nextlink href="" passHref scroll={false}>
-                        <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                            Submit
-                        </Button>
-                    </Nextlink>
-                </Box>
             </Section>
+
+            <Section>
+                <HookForm />
+            </Section>
+
+
+
         </Container>
     </Layout>
 )
